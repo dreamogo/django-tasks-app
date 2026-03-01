758406d4cf2ca2a7b20125ccb09134fbf6d2aed4
Nahashon Kimani <dreamogo@yahoo.com>
2026-03-01 15:26:42 -0800

    Switch to template rendering

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Your Django app is live 🚀")
+    return render(request, 'home.html')
